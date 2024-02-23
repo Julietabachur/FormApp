1850fdb3bb472395b810df57ede5a7652e03789c
Julieta BACHUR <jbachur@CNP.com.ar>
2024-02-23 15:03:55 -0300

    listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { ReactiveModule } from './reactive/reactive.module';
+import { SharedModule } from './shared/shared.module';
+import { RouterOutlet } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule,
+    ReactiveModule,
+    SharedModule,
+    RouterOutlet,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
